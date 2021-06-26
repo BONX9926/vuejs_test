c02b5ba66e481b6fba13bdb6d7cbb1377c9fe4f4
b.atiwat <winhackger@gmail.com>
2021-06-26 11:03:45 +0700

    Add navigation pane on the left with component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,42 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
-</template>
+<v-app>
+  <v-navigation-drawer app>
+    <navigation-left></navigation-left>
+  </v-navigation-drawer>
+  <v-app-bar app>
+    <!-- -->
+  </v-app-bar>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  <!-- Sizes your content based upon application components -->
+  <v-main>
+
+    <!-- Provides the application the proper gutter -->
+    <v-container fluid>
+
+      <!-- If using vue-router -->
+      <router-view></router-view>
+    </v-container>
+  </v-main>
+
+  <v-footer app>
+    <!-- -->
+  </v-footer>
+</v-app>
+</template>
+<script>
+import NavigationLeft from "@/components/NavigationLeft"
+export default {
+  components: {
+    NavigationLeft
+  }
 }
+</script>
+<style lang="scss">
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+// }
 </style>
